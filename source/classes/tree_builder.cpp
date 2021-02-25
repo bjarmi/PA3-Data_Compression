b59630300cafb04e7fd5438a48a8d55a2b8112b3
Maniacswimmer <sanderpall@hotmail.com>
2021-02-25 18:58:08 +0000

    Added the first iteration of TreeBuilder().

--- source/classes/tree_builder.cpp
+++ source/classes/tree_builder.cpp
@@ -9,12 +9,22 @@ struct TreeBuilder
 
 	static SaplingNode build()
 	{
-		while (heap.size() != 1)
+		while (heap.size() > 1)
 		{
-			break;
+			auto node_1 = heap.pop();
+			auto node_2 = heap.pop();
+			auto new_tree = SaplingNode(
+					"",
+					node_1.get_value() + node_2.value(),
+					node_1,
+					node_2
+			);
+
+			heap.push(new_tree);
+			build();
 		}
 
 		return heap.pop();
 	}
-};
+}
 
