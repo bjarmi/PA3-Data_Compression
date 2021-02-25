f265a3bf313ad41ebba820bdb045ebb2cfbd7867
Maniacswimmer <sanderpall@hotmail.com>
2021-02-25 17:59:15 +0000

    Reformatted for readability.

--- source/classes/sapling_node.cpp
+++ source/classes/sapling_node.cpp
@@ -3,25 +3,30 @@
 #include <utility>
 
 SaplingNode::SaplingNode(std::string key, int value, SaplingNode* left_child,
-                         SaplingNode* right_child) {
-    _key = std::move(key);
-    _value = value;
-    _left_child = left_child;
-    _right_child = right_child;
+                         SaplingNode* right_child)
+{
+	_key = std::move(key);
+	_value = value;
+	_left_child = left_child;
+	_right_child = right_child;
 }
 
-std::string SaplingNode::get_key() {
-    return _key;
+std::string SaplingNode::get_key()
+{
+	return _key;
 }
 
-int SaplingNode::get_value() const {
-    return _value;
+int SaplingNode::get_value() const
+{
+	return _value;
 }
 
-SaplingNode* SaplingNode::get_left_child() {
-    return _left_child;
+SaplingNode* SaplingNode::get_left_child()
+{
+	return _left_child;
 }
 
-SaplingNode* SaplingNode::get_right_child() {
-    return _right_child;
+SaplingNode* SaplingNode::get_right_child()
+{
+	return _right_child;
 }
