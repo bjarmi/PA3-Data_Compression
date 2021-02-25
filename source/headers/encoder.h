c90f9cad9025a8467dd9e32ee83019e519021e6d
Maniacswimmer <sanderpall@hotmail.com>
2021-02-25 16:35:48 +0000

    Added encoder header file.

---
+++ source/headers/encoder.h
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+#include <map>
+#include <string>
+
+#ifndef PA3_DATA_COMPRESSION_ENCODER_H
+#define PA3_DATA_COMPRESSION_ENCODER_H
+
+class Encoder
+{
+
+private:
+
+	string _input_file;
+	string _output_file
+
+	void encode();
+
+	std::priority_queue<SaplingNode> _build_frequency(std::string filename);
+
+	SaplingNode _build_tree(std::priority_queue<SaplingNode> heap);
+
+	std::map<std::string, std::string> _build_lexicon(SaplingNode tree);
+
+public:
+
+	Encoder(string input_file, string output_file);
+
+};
+
+#endif //PA3_DATA_COMPRESSION_ENCODER_H
