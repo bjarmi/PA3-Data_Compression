5c925c7ea13f5c720e92464c9b738e94efa40f87
bjarmi <bjarmiae@gmail.com>
2021-02-25 17:09:34 +0000

    Added Header file for Sapling node.

---
+++ source/headers/sapling_node.h
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#ifndef PA3_DATA_COMPRESSION_SAPLING_NODE_H
+#define PA3_DATA_COMPRESSION_SAPLING_NODE_H
+
+class SaplingNode {
+
+private:
+    std::string _key;
+    int _value;
+    SaplingNode _left_child;
+    SaplingNode _right_child;
+
+public:
+    SaplingNode(std::string key, int value, SaplingNode left_child,
+                SaplingNode right_node);
+
+    std::string get_key();
+
+    int get_value();
+
+    SaplingNode get_left_child();
+
+    SaplingNode get_right_child();
+
+};
+#endif //PA3_DATA_COMPRESSION_SAPLING_NODE_H
